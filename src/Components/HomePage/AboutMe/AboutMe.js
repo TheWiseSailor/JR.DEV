36a954428a77a98b8bcd9fbb306f823d845eff9e
TheWiseSailor <rudasilljay@gmail.com>
2024-04-01 12:14:41 -0400

    animation pt1

--- src/Components/HomePage/AboutMe/AboutMe.js
+++ src/Components/HomePage/AboutMe/AboutMe.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import './AboutMe.css'; // Import the CSS file
 import AMI1 from'./AboutMeImages/AMI1.png'
+import AOSInitializer from "../../AOS/AOSInitializer";
 const AboutMe = () => {
   return (
+
     <div className='AboutBG'>
-    <div className="about-me-container-primary">
-      
+  
+    <div className="about-me-container-primary"data-aos="fade-down" data-aos-duration="3000">
+    <AOSInitializer/>
       <div className="ABContainer">
-        <h2 className='H2Header'>About Me</h2>
-        <p className='AboutMeP'>
+      <AOSInitializer/>
+        <h2 className='H2Header'data-aos="fade-down" data-aos-duration="3000">About Me</h2>
+        <p className='AboutMeP'data-aos="fade-right" data-aos-duration="3000">
+        <AOSInitializer/>
 I am a Full-stack Developer who is heavily focused on the frontend. I love creating sleek, minimalist websites. In the future I would also like to take part, in both Real Estate, and Stock Market Exchanges,</p>
       </div>
-      <img src={AMI1} alt="Profile Picture" className="profile-picture" />
+   
+      <img src={AMI1} alt="Profile Picture" className="profile-picture" data-aos="fade-left" data-aos-duration="3000" />
+    
     </div>
     </div>
   );

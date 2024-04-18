6afd87893d5e3d81bbf2ea1371889190cfd0b2c9
TheWiseSailor <rudasilljay@gmail.com>
2024-04-18 19:16:10 -0400

    minor changes

--- src/Components/HomePage/AboutMe/AboutMe.js
+++ src/Components/HomePage/AboutMe/AboutMe.js
@@ -14,7 +14,7 @@ const AboutMe = () => {
         <h2 className='H2Header'data-aos="fade-down" data-aos-duration="3000">About Me</h2>
         <p className='AboutMeP'data-aos="fade-up" data-aos-duration="3000">
         <AOSInitializer/>
-I am a Full-stack Developer who is heavily focused on the frontend. I love creating sleek, minimalist websites. In the future I would also like to take part, in both Real Estate, and Stock Market Exchanges,</p>
+I am a Full-stack Developer who is heavily focused on the frontend. I love creating sleek, minimalist websites. In the future I would also like to take part, in both Real Estate, and Stock Market Exchanges.</p>
       </div>
    
       <img src={AMI1} alt="Profile Picture" className="profile-picture" data-aos="fade-down" data-aos-duration="3000" />
